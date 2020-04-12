599ac6d29af2c0e12c7e17d0649361bcffa3b396
SeraLee <cieroyou1@gmail.com>
2020-04-13 01:02:08 +0900

    jsonwebtoken 을 사용하여 Authentification 을 하는 미들웨어를 express-jwt 로 변경

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -1,42 +1,25 @@
-import { IUser } from './../interfaces/IUser';
 import { Request, Response, NextFunction } from 'express'
-import jwt from 'jsonwebtoken'
-import HttpException from '../exceptions/HttpException'
-// import jwt from 'express-jwt'
+import jwt from 'express-jwt'
+import dotenv from 'dotenv';
+dotenv.config();
 
 
-const isAuth = async (req: Request, res: Response, next: NextFunction) => {
+
+const getTokenFromHeader = (req: Request)=> {
+    // 사용자 요청 헤더에 jwt 토큰이 있는지 확인하고, 토큰을 return함
     const authHeader = req.headers['authorization']
     // const authHeader = req.headers.authorization; //Bearer
 
     //if we had authHeader, then return the authHeader.spilt(' ')[1]
-    const token = authHeader && authHeader.split(' ')[1]
-
-    if(token === null || token ===undefined){
-        next(new HttpException(401,'Not include AccessToken'));        
-    } 
-   
-    try{
-        const user = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        console.log('user', user);
-        req.user = user;
-        next();
-    }catch(err){
-        next(new HttpException(403, err.message))
+    const token_info = authHeader && authHeader.split(' ')[0];
+    if(token_info === 'Bearer' || token_info === 'Token'){
+        return authHeader.split(' ')[1];
     }
-  }
+    return null;
+}
 
-export default isAuth;
-
-// const getTokenFromHeader = () => {
-//     // 사용자 요청 헤더에 jwt 토큰이 있는지 확인하고, 토큰을 return함
-//     let jwt_token = '';
-//     console.log('isAuth - getTokenFromHeader')
-//     return jwt_token
-// }
-
-// export default jwt({
-//     secret : 'aaaa',
-//     userProperty: 'token',
-//     getToken: getTokenFromHeader
-// })
+export default jwt({
+    secret : process.env.ACCESS_TOKEN_SECRET,
+    userProperty: 'user',
+    getToken: getTokenFromHeader
+})
